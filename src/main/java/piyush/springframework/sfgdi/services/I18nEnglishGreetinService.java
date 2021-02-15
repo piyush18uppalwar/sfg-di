ec3188422aaff839428c872e87491e4100a344e0
Piyush <Piyush@DESKTOP-M0I4SOU>
2021-02-15 17:17:47 +0530

    Default Profile Commit

--- src/main/java/piyush/springframework/sfgdi/services/I18nEnglishGreetinService.java
+++ src/main/java/piyush/springframework/sfgdi/services/I18nEnglishGreetinService.java
@@ -3,7 +3,7 @@ package piyush.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class I18nEnglishGreetinService implements GreetingService {
 
